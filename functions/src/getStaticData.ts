f7a615b2e7eaccd44b488e2ad657b5d2faaa7d06
Andrew Dimmer <contactandrewd@gmail.com>
2019-10-13 03:35:39 -0400

    fix(functions): Allow all through CORS
    Makes it work for the demo, but is massively insecure. The current data does not necessitate data security, but clean this up in the future.

--- functions/src/getStaticData.ts
+++ functions/src/getStaticData.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 
 export const getGenresFunction = functions.https.onRequest(
   (request, response) => {
+    response.setHeader("Access-Control-Allow-Origin", "*"); // TODO: Make more secure later!
     response.send([
       { key: "rock", label: "Rock" },
       { key: "classical", label: "Classical" },
@@ -12,6 +13,7 @@ export const getGenresFunction = functions.https.onRequest(
 
 export const getArtistsFunction = functions.https.onRequest(
   (request, response) => {
+    response.setHeader("Access-Control-Allow-Origin", "*"); // TODO: Make more secure later!
     response.send([
       { key: "bob", label: "Bob" },
       { key: "joe", label: "Joe" },
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,7 @@ import { getGenresFunction, getArtistsFunction } from "./getStaticData";
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 export const helloWorld = functions.https.onRequest((request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*"); // TODO: Make more secure later!
   response.send("Hello from Firebase!");
 });
 
